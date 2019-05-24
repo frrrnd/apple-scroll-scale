1b8b2c3d03aa3ef8f94caee6546180138f5564f7
Fernando Soares <fernando.soares@sabia.com.br>
2019-05-24 16:55:52 -0300

    feature: effects, new video

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ var s = skrollr.init({
     forceHeight: false,
     smoothScrolling: true
 });
+
+
+fetch('video.mp4')
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  var objectURL = URL.createObjectURL(myBlob);
+  
+  const videos = document.getElementsByTagName("video")
+
+    for (video of videos) {
+        video.src = objectURL
+        video.play()
+    }
+});
